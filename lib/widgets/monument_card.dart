337c112356e53ef805871cfbe7d9e8d2113e5e29
Nicolas Draperi <nicolas.draperi2@gmail.com>
2024-08-07 15:17:24 +0200

    update monument_card

--- lib/widgets/monument_card.dart
+++ lib/widgets/monument_card.dart
@@ -5,7 +5,7 @@ import 'monument_detail.dart';
 class MonumentCard extends StatelessWidget {
   final Map<String, dynamic> monument;
 
-  MonumentCard(this.monument);
+  const MonumentCard(this.monument);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +13,7 @@ class MonumentCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      margin: EdgeInsets.symmetric(vertical: 10),
+      margin: const EdgeInsets.symmetric(vertical: 10),
       child: InkWell(
         onTap: () {
           Navigator.push(
@@ -22,21 +22,21 @@ class MonumentCard extends StatelessWidget {
           );
         },
         child: Container(
-          padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
           decoration: BoxDecoration(
-            color: Color(0xFFB9848C),
+            color: const Color(0xFFB9848C),
             borderRadius: BorderRadius.circular(10),
           ),
           child: Row(
             children: [
               Image.network(monument['image'],
                   width: 100, height: 100, fit: BoxFit.cover),
-              SizedBox(width: 10),
+              const SizedBox(width: 10),
               Expanded(
                 child: Text(
                   monument['name'],
                   style: GoogleFonts.numans(
-                    textStyle: TextStyle(
+                    textStyle: const TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
@@ -47,7 +47,7 @@ class MonumentCard extends StatelessWidget {
               Text(
                 'Distance: ${monument['distance']} km',
                 style: GoogleFonts.numans(
-                  textStyle: TextStyle(
+                  textStyle: const TextStyle(
                     fontSize: 16,
                     color: Colors.white,
                   ),
