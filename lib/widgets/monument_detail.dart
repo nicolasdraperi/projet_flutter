498d9654267f67679b1fdefc2186dad979b1bebb
Nicolas Draperi <nicolas.draperi2@gmail.com>
2024-08-08 15:21:10 +0200

    fdg

--- lib/widgets/monument_detail.dart
+++ lib/widgets/monument_detail.dart
@@ -14,46 +14,46 @@ class MonumentDetail extends StatelessWidget {
           monument['name'],
           style: GoogleFonts.firaSans(),
         ),
-        backgroundColor: Color(0xFF2F70AF),
+        backgroundColor: const Color(0xFF2F70AF),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.network(monument['image']),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             Text(
               monument['name'],
               style: GoogleFonts.firaSans(
-                textStyle: TextStyle(
+                textStyle: const TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             Text(
               monument['description'],
               style: GoogleFonts.numans(),
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             Text(
               'Distance: ${monument['distance']} km',
               style: GoogleFonts.numans(),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             // on va dire qui y a une API google maps quelque part qu'on pourrait foutre ici?
             Container(
               height: 200,
               color: Colors.grey[300],
-              child: Center(child: Text('gougar map')),
+              child: const Center(child: Text('gougar map')),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             Text(
               'User Reviews',
               style: GoogleFonts.firaSans(
-                textStyle: TextStyle(
+                textStyle: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
