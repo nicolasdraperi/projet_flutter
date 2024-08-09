a3848881f5bb303a917ad9e31bc856d1eacd1cc0
maggle <lenny.mallet@free.fr>
2024-08-09 04:34:03 +0200

    ajout monument
    ouais fais pas gaffe a l'heure

--- lib/monuments/monument_data.dart
+++ lib/monuments/monument_data.dart
@@ -13,8 +13,18 @@ class Louvre {
     'name': 'Le louvre',
     'image':
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFG_D5aaRP0wTYzeqi4KzWaq2e1O4kC8cl2Q&s',
-    'distance': '5',
+    'distance': '9',
     'description': "c'est juste un triangle chelou ",
+  };
+}
+
+class Seine {
+  static final data = {
+    'name': 'La Seine',
+    'image':
+        'https://upload.wikimedia.org/wikipedia/commons/2/26/Pont_au_Change_and_Palais_de_justice%2C_Paris_20_April_2021_001.jpg',
+    'distance': '1',
+    'description': "la hantise des nageurs olympiques ",
   };
 }
 // le saviez vous? les mini tours eiffel vendues en souvenir sont l'un des corps étrangers les plus extraits du corps humain aux urgences 
